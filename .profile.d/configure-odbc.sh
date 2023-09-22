961eba07d48d17a7105589cea8e1971e620d13a8
Jhedie <jeddiahawuku12@gmail.com>
2023-09-22 16:59:17 +0100

    chore: Change odbc version

--- .profile.d/configure-odbc.sh
+++ .profile.d/configure-odbc.sh
@@ -7,6 +7,6 @@ mkdir -p /etc
 
 echo "[ODBC Driver 18 for SQL Server]
 Description=Microsoft ODBC Driver 18 for SQL Server
-Driver=/app/.apt/usr/lib/libmsodbcsql-18.3.so.1.1
+Driver=/app/.apt/usr/lib/libmsodbcsql-18.0.so.1.1
 UsageCount=1
 " > ${ODBCSYSINI}/odbc.ini
